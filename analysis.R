dd8b52f8818ddb4d4c27dc5417d66e183174aac3
Ilya Kulikov <hedgef0g@users.noreply.github.com>
2020-10-05 23:36:20 +0300

    Initial commit

---
+++ analysis.R
@@ -0,0 +1,8 @@
+install.packages("tidyverse")
+install.packages("foreign")
+library(tidyverse)
+library(foreign)
+
+data <- read_csv("./DevEcosystem 20 external data sharing/2020_sharing_data_outside.csv")
+qre <- read_csv("./DevEcosystem 20 external data sharing/DevEcosystem 2020 questions_outside.csv")
+
